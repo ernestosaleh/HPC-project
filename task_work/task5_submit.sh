1a373d962f638b7727be717ca56952ba7fbff349
nikl <s243659@dtu.dk>
2025-04-16 19:43:22 +0200

    Tasks 1 to 5
    Details:
    Code, shell scripts and data files for the first 4 tasks (Task 5 is pending at the moment).
    
    Code for visualization can be found in messy_notebook.
    
    Task 2 shell script and code is committed, and results were saved in task1_to_3_result.npz (Execution time for 20 floor plans, together with u0, u_final (after running jacobi func until converged)) and interior_mask indices. For saving summary_statistics task1_to_3summary_statistics.csv was created.
    Task 4: Result from profile can be found in task4_24679602.out
    
    Task 5: Parallelization, but probably done wrong. It doesn't seem to work properly.

---
+++ task_work/task5_submit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#BSUB -J mini_proj_task_5
+#BSUB -q hpc
+#BSUB -W 24:00
+#BSUB -R "rusage[mem=2GB]"
+#BSUB -R "select[model==XeonGold6226R]"
+#BSUB -R "span[hosts=1]"
+#BSUB -n 32
+#BSUB -o task5_%J.out
+#BSUB -e task5_%J.err
+#BSUB -u s243659@dtu.dk
+#BSUB -N 
+#BSUB -K
+
+# Initialize Python environment 
+source /dtu/projects/02613_2025/conda/conda_init.sh
+conda activate 02613
+
+# Run Python script 
+python task5.py 40
